ecb35b29caee57e71414a9f735cf48e436899533
lolita89 <cika.lolita@hotmail.com>
2018-07-12 17:09:54 +0200

    Totally responsive mobile and desktop

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// var content = document.getElementById('content');
-// var title = document.getElementById('title-paragraph').innerHTML;
-// var paragraph = document.getElementById('paragraph-lorem').innerHTML;
-
-// console.log(title);
-// console.log(content);
-// console.log(paragraph);
-
-// for (i = 0; i < 3; i++) {
-//     content.innerHTML = content.innerHTML + title + "<br>" + paragraph;
-// }
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
